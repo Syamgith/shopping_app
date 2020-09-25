cebdd71f12dd30cb146ae84c6c729e9911049719
syamgith <syamgithksudheesh@gmail.com>
2020-09-26 00:57:34 +0530

    adding provider

---
+++ lib/providers/products.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  void addProduct() {
+    //add items
+    notifyListeners();
+  }
+}
