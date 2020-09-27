20d42621207b2ec42e2253dbd1c579055cf48f66
syamgith <syamgithksudheesh@gmail.com>
2020-09-27 19:29:23 +0530

    added fav using providers

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String id;
-  ProductItem(this.id, this.title, this.imageUrl);
-
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+    final imageUrl = product.imageUrl;
+    final title = product.title;
+    final id = product.id;
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -26,9 +27,14 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: Icon(
-            Icons.favorite,
-            color: Theme.of(context).accentColor,
+          leading: GestureDetector(
+            child: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: Theme.of(context).accentColor,
+            ),
+            onTap: () {
+              product.toggleFavorite();
+            },
           ),
           title: Text(
             title,
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
+import '../providers/product.dart';
 import 'product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
@@ -11,10 +12,15 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (context, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (context, i) => ChangeNotifierProvider(
+        create: (context) => Product(
+          id: products[i].id,
+          description: products[i].description,
+          title: products[i].title,
+          price: products[i].price,
+          imageUrl: products[i].imageUrl,
+        ),
+        child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
