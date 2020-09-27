04f7a621d1c951420350a24e7b057fd1207c4af4
syamgith <syamgithksudheesh@gmail.com>
2020-09-27 13:17:16 +0530

    used provider to disply title in details

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-details';
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((prod) => prod.id == productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: Text(loadedProduct.title),
       ),
     );
   }
