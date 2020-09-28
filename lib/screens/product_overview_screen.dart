576a2cd71567f2ee4cd265cc9c5053eb83b9e829
syamgith <syamgithksudheesh@gmail.com>
2020-09-28 18:06:35 +0530

    adding popupmenubutton

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import '../widgets/products_grid.dart';
 
+enum filterOptions { favorites, all }
+
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (filterOptions value) {
+              print(value);
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: filterOptions.favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: filterOptions.all,
+              )
+            ],
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
