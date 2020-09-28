95265de43bb678a50c0c759967997693312cf4ac
syamgith <syamgithksudheesh@gmail.com>
2020-09-29 01:45:54 +0530

    Added cart provider usg multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/products.dart';
+import 'providers/cart.dart';
 import 'screens/product_overview_screen.dart';
 import 'screens/product_detail_screen.dart';
 
@@ -12,8 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
