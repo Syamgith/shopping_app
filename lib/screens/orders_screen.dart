cd082c87b182272ecd1b509d3019ddeb3ad745be
syamgith <syamgithksudheesh@gmail.com>
2020-10-04 12:35:33 +0530

    auth

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,36 +4,36 @@ import '../widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/order_scrren';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) {
-      Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context);
+    //final ordersData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: ordersData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(
-          ordersData.orders[i],
-        ),
-      ),
+      body: FutureBuilder<Object>(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('An error occured.'));
+            }
+            return Consumer<Orders>(
+              builder: (context, ordersData, child) => ListView.builder(
+                itemCount: ordersData.orders.length,
+                itemBuilder: (ctx, i) => OrderItem(
+                  ordersData.orders[i],
+                ),
+              ),
+            );
+          }),
     );
   }
 }
